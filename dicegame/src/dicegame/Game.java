c2418759f0cc0224ae6c5d6222a771921b842dbc
andre <andre@DESKTOP-R41QR2C>
2019-09-07 14:41:26 -0500

    added the ability for people to pick between 1 dice or 2 dice with a
    yes or no option.

--- dicegame/src/dicegame/Game.java
+++ dicegame/src/dicegame/Game.java
@@ -5,6 +5,12 @@ public class Game {
   
    public void play() {
    
+	   Scanner myObj = new Scanner(System.in);
+	   System.out.println("would you like to play for 5 times the amount yes or no?");
+	   String userinput = myObj.nextLine();
+	 	   
+	String yes =  "yes";
+	if (yes.equals(userinput)) {  
       // get input from user
       Scanner scnr = new Scanner(System.in);
       System.out.print("How much do you want to bet? ");                                                 
@@ -28,10 +34,35 @@ public class Game {
     	  System.out.println("you lose");
       }
         
+	}
+	
+	else {
+	
+		Scanner scnr = new Scanner(System.in);
+	      System.out.print("How much do you want to bet? ");                                                 
+	      int enteredBet = scnr.nextInt(); 
+	      System.out.print("What number do you choose ? ");
+	      int enteredNum = scnr.nextInt(); 
+	   
+	      // generate the die roll 
+	      Random randGen = new Random();
+	      int dieNum1  = randGen.nextInt(6) + 1;
+	     
+	      System.out.println("The die is a " + dieNum1 );
+	      
+	      
+	      if ( enteredBet == dieNum1) {
+	    	  int amountwon = enteredBet * 10;
+	    	  System.out.println("you win " + amountwon+" dollars!");    	  
+	      }
+	      
+	      else {   	  
+	    	  System.out.println("you lose");
+	      }
+		
+		
+	}
       
       
-      
-      // write the rest of your code below
-        
    }
 }
